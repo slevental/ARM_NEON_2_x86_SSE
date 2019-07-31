4fe95446bff6a91dadd376b11efd58e20765080d
slevental <stanislav.levental@gmail.com>
2019-07-31 15:55:39 -0700

    fixing type issue for square root operation

--- NEON_2_SSE.h
+++ NEON_2_SSE.h
@@ -6854,6 +6854,35 @@ _NEON2SSE_INLINE float32x2_t vrsqrte_f32(float32x2_t a) //use low 64 bits
     return res64;
 }
 
+
+_NEON2SSESTORAGE uint32x2_t vrsqrte_u32_old(uint32x2_t a); // VRSQRTE.U32 d0,d0
+_NEON2SSE_INLINE _NEON2SSE_PERFORMANCE_WARNING(uint32x2_t vrsqrte_u32_old(uint32x2_t a), _NEON2SSE_REASON_SLOW_SERIAL)
+{
+  //Input is  fixed point number!!!
+  //We implement the recip_sqrt_estimate function as described in ARMv7 reference manual (VRSQRTE instruction) but use float instead of double
+  uint32x2_t res;
+  __m128 tmp;
+  float r, resf, coeff;
+  int i,q0, s;
+  for (i =0; i<2; i++){
+    if((a.m64_u32[i] & 0xc0000000) == 0) { //a <=0x3fffffff
+      res.m64_u32[i] = 0xffffffff;
+    }else{
+      resf =  (float) (a.m64_u32[i] * (0.5f / (uint32_t)(1 << 31)));
+      coeff = (resf < 0.5f)? 512.0f : 256.0f ; /* range 0.25 <= resf < 0.5  or range 0.5 <= resf < 1.0*/
+      q0 = (int)(resf * coeff); /* a in units of 1/512 rounded down */
+      r = ((float)q0 + 0.5f) / coeff;
+      tmp = _mm_rsqrt_ss(_mm_load_ss( &r));/* reciprocal root r */
+      _mm_store_ss(&r, tmp);
+      s = (int)(256.0f * r + 0.5f); /* r in units of 1/256 rounded to nearest */
+      r = (float)(s / 256.0f);
+      res.m64_u32[i] = (uint32_t)(r * (((uint32_t)1) << 31));
+    }
+  }
+  return res;
+}
+
+
 _NEON2SSESTORAGE uint32x2_t vrsqrte_u32(uint32x2_t a); // VRSQRTE.U32 d0,d0
 _NEON2SSE_INLINE _NEON2SSE_PERFORMANCE_WARNING(uint32x2_t vrsqrte_u32(uint32x2_t a), _NEON2SSE_REASON_SLOW_SERIAL)
 {
@@ -6863,7 +6892,8 @@ _NEON2SSE_INLINE _NEON2SSE_PERFORMANCE_WARNING(uint32x2_t vrsqrte_u32(uint32x2_t
   // from ARM implementation due to _mm_rsqrt_ps precision
   uint32x2_t res;
   int i;
-  _NEON2SSE_ALIGN_16 float rr[2], coeff[2];
+  float32x2_t rr;
+  _NEON2SSE_ALIGN_16 float coeff[2];
   union {
     double d[2];
     uint64_t u64[2];
@@ -6899,13 +6929,13 @@ _NEON2SSE_INLINE _NEON2SSE_PERFORMANCE_WARNING(uint32x2_t vrsqrte_u32(uint32x2_t
   s_f = _mm_cvtepi32_ps(_mm_cvttps_epi32(s_f));
 #endif
   s_f = _mm_div_ps(s_f, c256_f);
-  _M64f(rr[0], s_f);
+  _M64f(rr, s_f);
 
   for (i = 0; i < 2; i++) {
     if ((a.m64_u32[i] & 0xc0000000) == 0) { // a <=0x3fffffff
       res.m64_u32[i] = 0xffffffff;
     } else {
-      res.m64_u32[i] = rr[i] * (((uint32_t)1) << 31);
+      res.m64_u32[i] = rr.m64_f32[i] * (((uint32_t)1) << 31);
     }
   }
   return res;
